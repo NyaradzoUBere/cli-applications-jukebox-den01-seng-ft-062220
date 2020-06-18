a3bedcf03bc5fe5a0bdaf961a3cd42a82d908635
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-18 01:34:00 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,7 +32,7 @@ def play(x)
   puts "Please enter a song name or number:"
   user_input = gets.chomp
   if user_input == (1..9).to_s
-    puts "Playing #{x[user_input - 1]}"
+    puts "Playing #{x[user_input]}"
   else
     puts "Invalid input, please try again"
   end
